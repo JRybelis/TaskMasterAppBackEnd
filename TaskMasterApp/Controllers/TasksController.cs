1184fc836c69949b4c0613869a08e447880d279c
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-03 21:21:55 +0300

    Create Task model and TaskController. Add GetAll method to the controller.

---
+++ TaskMasterApp/Controllers/TasksController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TaskMasterApp.Models;
+
+namespace TaskMasterApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Task> Get()
+        {
+            return new List<Task>()
+            {
+                new Task()
+                {
+                    Id = 1,
+                    Text = "Buy milk",
+                    Day = "Wednesday",
+                    Reminder = false                    
+                },
+                new Task()
+                {
+                    Id = 2,
+                    Text = "Do chores",
+                    Day = "Tuesday",
+                    Reminder = true                    
+                }
+            };
+        }
+    }
+}
---
+++ TaskMasterApp/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMasterApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Day { get; set; }
+        public bool Reminder { get; set; } = false;
+    }
+}
