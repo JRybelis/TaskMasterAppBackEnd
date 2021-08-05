bc18a06da81b116863353e758309376fb9817c57
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-05 16:52:13 +0300

    Add and register default database connection string.

--- TaskMasterApp/Startup.cs
+++ TaskMasterApp/Startup.cs
@@ -26,6 +26,8 @@ namespace TaskMasterApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var defaultConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
+
             services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
