957d84b9cc2cef3e3e1dc12709d19d9aee98a6fe
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-05 18:17:08 +0300

    Move Models to TaskmasterCore project. Add TaskDto.

---
+++ TaskmasterCore/Dtos/TaskDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskmasterCore.Dtos
+{
+    public abstract class TaskDto
+    {
+        [MaxLength(511)] public string Text { get; set; }
+        [MaxLength(9)]public string Day { get; set; }
+        public bool Reminder { get; set; } = false;
+
+    }
+}
