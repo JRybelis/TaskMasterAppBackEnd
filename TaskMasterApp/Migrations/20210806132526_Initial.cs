8eb2f4fca4f2b060ac431578c68970050cb449ec
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-06 16:29:15 +0300

    Initial migration

---
+++ TaskMasterApp/Migrations/20210806132526_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskMasterApp.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(511)", maxLength: 511, nullable: false),
+                    Day = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    Reminder = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
