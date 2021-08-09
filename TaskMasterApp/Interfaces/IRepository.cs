3a68fae34a3b4f3ca938bbcc73869a92ba80c243
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-09 18:38:09 +0300

    Add data repository interface and repository.

---
+++ TaskMasterApp/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;s
+using TaskmasterCore.Models;
+
+namespace TaskMasterApp.Interfaces
+{
+    public interface IRepository
+    {
+        Task<List<TaskItem>> GetAll();
+        Task<TaskItem> GetById(int id);
+        Task Upsert(TaskItem entity);
+        Task Delete(int id);
+    }
+}
---
+++ TaskMasterApp/Repositories/Repository.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskMasterApp.Data;
+using TaskMasterApp.Interfaces;
+using TaskmasterCore.Models;
+
+namespace TaskMasterApp.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly DataContext _context;
+
+        public Repository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<List<TaskItem>> GetAll()
+        {
+            return await _context.Set<TaskItem>().ToListAsync();
+        }
+
+        public async Task<TaskItem> GetById(int id)
+        {
+            var entity = await _context.Set<TaskItem>().FirstOrDefaultAsync(e => e.Id == id);
+
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            return entity;
+        }
+
+        public async Task Upsert(TaskItem entity)
+        {
+            if (entity.Id != 0)
+            {
+                _context.Update(entity);
+            }
+            else
+            {
+                _context.Add(entity);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var entity = await _context.Set<TaskItem>().FirstOrDefaultAsync(e => e.Id == id);
+
+            if (entity != null)
+            {
+                _context.Remove(entity);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
