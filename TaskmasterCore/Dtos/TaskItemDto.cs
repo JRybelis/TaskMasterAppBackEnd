5b002a1a7b2461f2079c397def34d6c24b390b03
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-11 15:55:31 +0300

    Remove abstract class modifier.

--- TaskmasterCore/Dtos/TaskItemDto.cs
+++ TaskmasterCore/Dtos/TaskItemDto.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace TaskmasterCore.Dtos
 {
-    public abstract class TaskItemDto
+    public class TaskItemDto
     {
         [MaxLength(511)] public string Text { get; set; }
         [MaxLength(9)]public string Day { get; set; }
