a2de47e99abf7108f2c5c01af0a0834a1b7e5ce5
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-08-11 19:00:55 +0300

    Create TaskMastersTests project | Add DataSeeder & CustomWebApplicationFactory for in-memory db seeding.

---
+++ TaskMasterTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TaskMasterApp.Data;
+
+namespace TaskMasterTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                services.Remove(descriptor);
+                services.AddDbContext<DataContext>(options =>
+                {
+                    string _dbName = Guid.NewGuid().ToString();
+
+                    options.UseInMemoryDatabase(_dbName);
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<DataContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        DataSeeder.SeedData(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex,
+                            "An error occurred seeding the " + " database with test messages. Error: {Message}",
+                            ex.Message);
+                        throw;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ TaskMasterTests/DataSeeder.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using TaskMasterApp.Data;
+using TaskmasterCore.Models;
+
+namespace TaskMasterTests
+{
+    public static class DataSeeder
+    {
+        public static void SeedData(DataContext context)
+        {
+            if (!context.Tasks.Any())
+            {
+                context.Tasks.Add(new TaskItem()
+                {
+                    Text = "Test task item number 1",
+                    Day = "Sunday",
+                    Reminder = false
+                });
+
+                context.SaveChangesAsync();
+
+            }
+        }
+    }
+}
